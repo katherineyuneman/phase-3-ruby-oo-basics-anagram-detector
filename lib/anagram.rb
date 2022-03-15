a7c943784eddeefdb07356583fe24189bc0c5f35
katherineyuneman <kyuneman@gmail.com>
2022-03-15 13:52:27 -0400

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+
+        map = array.map { |element| element }
+        filter = map.select {|letters| letters.chars.sort == @word.chars.sort}
+
+    end
+end
+
+
